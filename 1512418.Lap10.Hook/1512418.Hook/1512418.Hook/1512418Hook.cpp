7b2dbc6a964dfe29de19c1632df0a29586ae8aee
Duy Phuong <tranduyphuong20100@gmail.com>
2017-12-01 17:18:53 +0700

    lap 10

---
+++ 1512418.Lap10.Hook/1512418.Hook/1512418.Hook/1512418Hook.cpp
@@ -0,0 +1,267 @@
+﻿// 1512418Hook.cpp : Defines the entry point for the application.
+//
+
+#include "stdafx.h"
+#include "1512418Hook.h"
+
+#define MAX_LOADSTRING 100
+
+// Global Variables:
+HINSTANCE hInst;                                // current instance
+WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
+WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
+HINSTANCE hDLL;
+int shift = 0;//an shift
+int hold = 0;
+
+void doInstallHook(HWND);
+void doRemoveHook(HWND);
+bool i = false; //chua bam 
+
+// Forward declarations of functions included in this code module:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: Place code here.
+
+    // Initialize global strings
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_1512418HOOK, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // Perform application initialization:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_1512418HOOK));
+
+    MSG msg;
+
+    // Main message loop:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+
+
+//
+//  FUNCTION: MyRegisterClass()
+//
+//  PURPOSE: Registers the window class.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_1512418HOOK));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_1512418HOOK);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   FUNCTION: InitInstance(HINSTANCE, int)
+//
+//   PURPOSE: Saves instance handle and creates main window
+//
+//   COMMENTS:
+//
+//        In this function, we save the instance handle in a global variable and
+//        create and display the main program window.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // Store instance handle in our global variable
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+//
+//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  PURPOSE:  Processes messages for the main window.
+//
+//  WM_COMMAND  - process the application menu
+//  WM_PAINT    - Paint the main window
+//  WM_DESTROY  - post a quit message and return
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+
+    switch (message)
+    {
+	case WM_SYSKEYDOWN:
+		if(hold == 0)
+		{
+			//MessageBox(0, L"DOWN ALT", 0, 0);
+			
+			hold = 1;
+
+		}
+		else
+		{
+			//MessageBox(0, L"DOWN UP", 0, 0);
+
+			doRemoveHook(hWnd);
+
+			hold = 0;
+
+		}
+		break;
+
+    case WM_COMMAND:
+        {
+            int wmId = LOWORD(wParam);
+            // Parse the menu selections:
+            switch (wmId)
+            {
+			case ID_INSTALL_HOOK:
+			{
+				doInstallHook(hWnd);
+				if (hold == 1)
+				{
+					MessageBox(hWnd, L"You have already installed hook\r\nPress CTRL + S to swap mouse button\r\nPress ALT to uninstall hook", L"Result", MB_OK);
+
+				}
+				hold = 1;
+
+				break;
+			}
+				
+            case IDM_ABOUT:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                break;
+            case IDM_EXIT:
+				SwapMouseButton(FALSE);//tro ve bt
+				doRemoveHook(hWnd);
+
+                DestroyWindow(hWnd);
+                break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+            // TODO: Add any drawing code that uses hdc here...
+            EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_DESTROY:
+		SwapMouseButton(FALSE);//tro ve bt
+		doRemoveHook(hWnd);
+
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// Message handler for about box.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
+
+void doInstallHook(HWND hWnd)
+{
+	// gọi hàm DLL theo kiểu Run-time
+	// Định nghĩa prototype của hàm
+	typedef VOID(*MYPROC)(HWND);
+
+	HINSTANCE hinstLib;
+	MYPROC ProcAddr;
+
+	// load DLL và lấy handle của DLL module
+	hinstLib = LoadLibrary(L"dllmain.dll");
+	// Nếu load thành công, lấy địa chỉ của hàm  trong DLL
+	if (hinstLib != NULL) {
+		ProcAddr = (MYPROC)GetProcAddress(hinstLib, "_doInstallHook");
+		// Nếu lấy được địa chỉ hàm, gọi thực hiện hàm
+		if (ProcAddr != NULL)
+			ProcAddr(hWnd);
+
+	}
+}
+
+void doRemoveHook(HWND hWnd)
+{
+	// gọi hàm DLL theo kiểu Run-time
+	// Định nghĩa prototype của hàm
+	typedef VOID(*MYPROC)(HWND);
+
+	HINSTANCE hinstLib;
+	MYPROC ProcAddr;
+
+	// load DLL và lấy handle của DLL module
+	hinstLib = LoadLibrary(L"dllmain.dll");
+	// Nếu load thành công, lấy địa chỉ của hàm DrawCircle trong DLL
+	if (hinstLib != NULL) {
+		ProcAddr = (MYPROC)GetProcAddress(hinstLib, "_doRemoveHook");
+		// Nếu lấy được địa chỉ hàm, gọi thực hiện hàm
+		if (ProcAddr != NULL)
+			ProcAddr(hWnd);
+	}
+}
---
+++ 1512418.Lap10.Hook/1512418.Hook/dllmain/dllmain.cpp
@@ -0,0 +1,84 @@
+﻿// dllmain.cpp : Defines the entry point for the DLL application.
+#include "stdafx.h"
+
+#define EXPORT __declspec(dllexport) 
+
+bool i = false;
+HHOOK hHook = NULL;
+HINSTANCE hinstLib;
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+	DWORD  ul_reason_for_call,
+	LPVOID lpReserved
+)
+{
+	hinstLib = hModule;
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+}
+
+
+LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
+{
+
+	PKBDLLHOOKSTRUCT p = (PKBDLLHOOKSTRUCT)lParam;
+	if (nCode < 0) // không xử lý message 
+		return CallNextHookEx(hHook, nCode, wParam, lParam);
+	// xử lý message: Ctrl + s		
+	if (nCode == HC_ACTION)
+	{
+		if (wParam == WM_KEYDOWN)
+		{
+			if ((p->vkCode == 0x53) && GetAsyncKeyState(VK_CONTROL))
+			{
+				if (i == true)
+				{
+
+					i = false;
+					SwapMouseButton(i);
+					MessageBox(0, L"Bạn sẽ chuyển chuột trái thành chuột phải bình thường như ban đầu", L"Thông báo", MB_OK);
+
+				}
+				else
+				{
+					MessageBox(0, L"Bạn sẽ chuyển chuột phải thành chuột trái", L"Thông báo", MB_OK);
+					i = true;
+					SwapMouseButton(i);
+
+				}
+
+				return TRUE;
+			}
+		}
+		
+	}
+	return CallNextHookEx(0, nCode, wParam, lParam);
+}
+extern "C"
+{
+	EXPORT void _doInstallHook(HWND hWnd)
+	{
+		if (hHook != NULL) return;
+
+		hHook = SetWindowsHookEx(WH_KEYBOARD_LL, (HOOKPROC)KeyboardProc, hinstLib, 0);
+		if (hHook)
+			MessageBox(hWnd, L"Setup hook successfully \r\nPress CTRL + S to swap mouse button\r\nPress ALT to uninstall hook", L"Result", MB_OK);
+		else
+			MessageBox(hWnd, L"Setup hook fail", L"Result", MB_OK);
+	}
+
+	EXPORT void _doRemoveHook(HWND hWnd)
+	{
+		if (hHook == NULL) return;
+		UnhookWindowsHookEx(hHook);
+		hHook = NULL;
+		MessageBox(hWnd, L"Remove hook successfully", L"Result", MB_OK);
+	}
+}
